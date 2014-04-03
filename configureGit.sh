44605b8b39a24d907511a4110c16757dd48dffa5
Conrad Pearson <cspearso@calpoly.edu>
2014-04-03 14:02:30 -0700

    adding ignore file and config script JLK

---
+++ configureGit.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script configured git whenever you start working on multiple machines.
+# Run it any time you decide to work with it on a new computer
+
+git config --global user.name "Conrad Pearson"
+git config --global user.email "cspearso@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor  "nano"
